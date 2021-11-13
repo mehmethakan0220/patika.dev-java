f7587e869768f41437aaf859d9a07829592253df
mehmethakan0220 <mehmethakan0220@gmail.com>
2021-11-14 00:08:16 +0300

    for dongusunden yerine while dongusu kullanildi

--- src/EbobEkok/EbobEkok.java
+++ src/EbobEkok/EbobEkok.java
@@ -7,27 +7,20 @@ public class EbobEkok {
         Scanner scanner = new Scanner(System.in);
         int n1;
         int n2;
-        int greater;
-        int lesser;
+
         int ebob =0;
         int ekok =0;
-        System.out.println("Ebob Ekok bulalim.!");
 
         System.out.print("Ilk sayiyi giriniz.\t:");
         n1 = scanner.nextInt();
         System.out.print("Ikinci sayiyi giriniz.\t:");
         n2 = scanner.nextInt();
 
-        if(n1>n2){
-            greater = n1;
-            lesser = n2;
-        }else{
-            greater = n2;
-            lesser = n1;
-        }
 
-        for(int i=1;i<greater;i++){
-            if(i<=lesser){
+        int i =0;
+        while(true){
+            i++;
+            if(i<=n1 && i<=n2){
                 if( (n1%i ==0) && (n2%i ==0)){
                     System.out.println("ortak bolen\t:"+i);
                     ebob = i;
@@ -35,6 +28,7 @@ public class EbobEkok {
             }else {
                 break;
             }
+
         }
 
         System.out.println(n1+" ve "+n2+" sayilarinin EBOB'u\t:"+ebob);
